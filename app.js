f88fa9011c635736171e49a85eac6cc4720bf8e0
KolliOmkar <kolliomkar0@gmail.com>
2026-05-09 12:33:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,33 @@
 function showLogin(){
+
   splash.classList.add("hidden");
+
   login.classList.remove("hidden");
 }
 
 function toggleMenu(){
+
   menu.classList.toggle("hidden");
+}
+
+function showProfile(){
+
+  alert(
+    "Customer Profile"
+  );
+}
+
+function showHelp(){
+
+  alert(
+    "Chat with admin:\nhelp@homnix.com"
+  );
+}
+
+function bookService(service){
+
+  alert(
+    service +
+    " booked successfully"
+  );
 }
